e6d4df202fc558333866f25232d6e3ce08d54dcb
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-07-29 15:47:17 -0300

    ajuste na table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,6 +1,8 @@
-/* eslint-disable react/no-array-index-key */
+/* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { useEffect, useState } from 'react';
+import {
+  useCallback, useEffect, useMemo, useState,
+} from 'react';
 import { Container } from './styles';
 
 import { api } from '../../services/api';
@@ -8,20 +10,43 @@ import { onlyUnique } from '../../utils/onlyUnique';
 
 export function MoviesTable() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [genreSelected, setGenreSelected] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
   const [optionsGenre, setOptionsGenre] = useState([]);
 
-  useEffect(() => {
-    async function getData() {
+  const filteredMovies = useMemo(() => movies.filter((movie) => {
+    if (searchTerm.length > 0 && genreSelected.length === 0) {
+      console.log('searchTerm tá escrito e genero não selecionado');
+      const filteredBySearchTerm = movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+      return filteredBySearchTerm;
+    } if (searchTerm.length > 0 && genreSelected.length > 0) {
+      console.log('searchTerm tá escrito e genero ESTÁ selecionado');
+      const filteredBySearchTerm = movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+      const filteredByGenre = filteredBySearchTerm.genre?.some((g) => g.toLowerCase() === genreSelected);
+      return filteredByGenre;
+    } if (searchTerm.length === 0 && genreSelected.length > 0) {
+      console.log('searchTerm NÃO tá escrito e genero ESTÁ selecionado');
+      const filteredByGenre = movies.genre?.some((g) => g.toLowerCase() === genreSelected);
+      return filteredByGenre;
+    }
+
+    return movie;
+  }), [movies, searchTerm, genreSelected]);
+
+  const loadMovies = useCallback(async () => {
+    setIsLoading(true);
+
+    try {
       const response = await api.get('/movies');
 
       const genres = [];
-
       const dataFormatted = response.data.map((movie, index) => {
         movie.genre.forEach((g) => genres.push(g));
         return {
           ...movie,
           id: index + Math.random(),
-          revenueFormatted: `$${movie.revenue}`,
+          revenueFormatted: movie.revenue.length > 0 ? `$${movie.revenue}` : '$0.00',
           genreFormatted: movie.genre.join(', '),
         };
       });
@@ -29,13 +54,39 @@ export function MoviesTable() {
       const unique = genres.filter(onlyUnique);
       setOptionsGenre(unique);
       setMovies(dataFormatted);
+    } catch {
+      setMovies([]);
+    } finally {
+      setIsLoading(false);
     }
-
-    getData();
   }, []);
+
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  useEffect(() => {
+    loadMovies();
+  }, [loadMovies]);
 
   return (
     <Container>
+      <p>
+        genreSelected:
+        {genreSelected}
+      </p>
+      <p>
+        genreSelectedLength:
+        {genreSelected.length}
+      </p>
+      <p>
+        searchTerm:
+        {searchTerm}
+      </p>
+      <p>
+        searchTermLength:
+        {searchTerm.length}
+      </p>
       <table>
         <thead>
           <tr>
@@ -48,15 +99,26 @@ export function MoviesTable() {
           </tr>
           <tr>
             <th>
-              <input type="text" placeholder="Filter by title" />
+              <input
+                value={searchTerm}
+                type="text"
+                placeholder="Filter by title"
+                onChange={handleChangeSearchTerm}
+                className="inputFilter"
+              />
             </th>
             <th />
             <th />
             <th />
             <th />
             <th>
-              <select name="genres">
-                <option value="all">All</option>
+              <select
+                name="genres"
+                className="genreFilter"
+                value={genreSelected}
+                onChange={(event) => setGenreSelected(event.target.value)}
+              >
+                <option value="">All</option>
                 {optionsGenre.map((optionGenre) => (
                   <option value={optionGenre.toLowerCase()}>{optionGenre}</option>
                 ))}
@@ -64,18 +126,34 @@ export function MoviesTable() {
             </th>
           </tr>
         </thead>
-        <tbody>
-          {movies.map((movie) => (
-            <tr key={movie.id}>
-              <td>{movie.title}</td>
-              <td>{movie.year}</td>
-              <td>{movie.runtime}</td>
-              <td>{movie.revenueFormatted}</td>
-              <td>{movie.rating}</td>
-              <td>{movie.genreFormatted}</td>
-            </tr>
-          ))}
-        </tbody>
+        {movies.length > 0 ? (
+          <tbody>
+            {filteredMovies.map((movie) => (
+              <tr key={movie.id}>
+                <td>{movie.title}</td>
+                <td>{movie.year}</td>
+                <td>{movie.runtime}</td>
+                <td>{movie.revenueFormatted}</td>
+                <td>{movie.rating}</td>
+                <td>{movie.genreFormatted}</td>
+              </tr>
+            ))}
+          </tbody>
+
+        ) : isLoading ? (
+          <div>
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <div>
+            <h1>
+              Theres nothing like
+              <strong>
+                {searchTerm}
+              </strong>
+            </h1>
+          </div>
+        )}
       </table>
     </Container>
   );
